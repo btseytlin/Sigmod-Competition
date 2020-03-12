5cfdc66ea5e9648d5468993aae42022147b3141f
Борис Цейтлин <euphetar@gmail.com>
2020-03-12 13:06:53 +0100

    Add working triplet loss

--- sigmod_src/data/embedding.py
+++ sigmod_src/data/embedding.py
@@ -1,41 +1,90 @@
 import os
-import pickle
+import dill
 import gensim
 import nltk
-from sklearn.model_selection import train_test_split
-from gensim.similarities.index import AnnoyIndexer
-from gensim.models.doc2vec import Doc2Vec
-from gensim.models.callbacks import CallbackAny2Vec
+import torch
 
+import torchtext
+import torch.nn as nn
+import torch.nn.functional as F
+from torchtext.data import Field
 
-def tokenize_doc(doc):
-    return nltk.tokenize.word_tokenize(doc)
+from annoy import AnnoyIndex
 
-def tokenize_docs(docs):
-    for doc in docs:
-        yield tokenize_doc(doc)
+def tokenize(text):
+    return text.split()
 
-def tag_docs(docs, ids):
-    for doc_id, doc in zip(ids, docs):
-        tokens = tokenize_doc(doc)
-        yield gensim.models.doc2vec.TaggedDocument(tokens, [doc_id])
+def get_fields():
+    text_field = Field(sequential=True, tokenize=tokenize, lower=True)
+    return text_field
+
+class LSTMEmbedder(nn.Module):
+    def __init__(self,
+                 text_field=None,
+                 hidden_dim=100,
+                 emb_dim=100,
+                 spatial_dropout=0.05, 
+                 recurrent_dropout=0.1, 
+                 num_linear=1,
+                 num_lstm=1,
+                 **kwargs):
+        super().__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.emb_dim = emb_dim
+        self.spatial_dropout = spatial_dropout
+        self.recurrent_dropout = recurrent_dropout
+        self.num_linear = num_linear
+        self.num_lstm = num_lstm
+
+        self.text_field = text_field
+
+        self.embedding = nn.Embedding(len(text_field.vocab), emb_dim)
+        self.encoder = nn.LSTM(emb_dim, hidden_dim,
+                               num_layers=num_lstm, 
+                               dropout=recurrent_dropout)
+        self.linear_layers = []
+        for _ in range(num_linear):
+            self.linear_layers.append(nn.Linear(hidden_dim, hidden_dim))
+        self.linear_layers = nn.ModuleList(self.linear_layers)
+
+    def configuration_dict(self):
+        return dict(hidden_dim=self.hidden_dim,
+                    emb_dim=self.emb_dim,
+                    spatial_dropout=self.spatial_dropout,
+                    recurrent_dropout=self.recurrent_dropout,
+                    num_linear=self.num_linear,
+                    num_lstm=self.num_lstm)
+
+    def forward(self, seq):
+        hdn, _ = self.encoder(self.embedding(seq))
+        feature = hdn[-1, :, :]
+        for layer in self.linear_layers:
+            feature = layer(feature)
+        return feature
+    
+    def infer(self, texts):
+        tokens = [tokenize(t) for t in texts]
+        tensor = self.text_field.numericalize(tokens)
+        return self(tensor)
+
 
 class Embedder:
-    def __init__(self, vector_size=200,
-                       train_epochs=300,
-                       index_trees=1000):
-        self.vector_size = vector_size
-        self.train_epochs = train_epochs
+    def __init__(self, 
+                 model=None,
+                 ids=None,
+                 texts=None,
+                 index_trees=300):
+        self.model = model
         self.index_trees = index_trees
-        self.texts = []
-        self.ids = []
-
-        self.doc2vec = None
+        self.ids = ids
+        self.texts = texts
+        self.emb_dim = None
         self.indexer = None
 
     def get_params_dict(self):
-        return dict(vector_size=self.vector_size,
-                train_epochs=self.train_epochs,
+        return dict(
+                emb_dim=self.emb_dim,
                 index_trees=self.index_trees,
                 texts=self.texts,
                 ids=self.ids)
@@ -43,62 +92,58 @@ class Embedder:
     def set_params_dict(self, params):
         for k, v in params.items():
             setattr(self, k, v)
+    
+    def make_indexer(self, embeddings):
+        indexer = AnnoyIndex(len(embeddings[0]), 'angular')
 
-    def fit(self, texts, ids):
-        train_corpus = list(tag_docs(texts, ids))
+        for i, emb in enumerate(embeddings):
+            indexer.add_item(i, emb)
 
-        doc2vec = Doc2Vec(vector_size=self.vector_size,
-                            min_count=2,
-                            epochs=self.train_epochs,
-                            workers=4)
+        indexer.build(10000)
+        return indexer
 
-        doc2vec.build_vocab(train_corpus)
-
-        class EpochLogger(CallbackAny2Vec):
-            def __init__(self):
-                self.epoch = 0
-
-            def on_epoch_end(self, model):
-                self.epoch += 1
-                if self.epoch % 10 == 0:
-                    print(f"Epoch #{self.epoch}")
-
-        doc2vec.train(train_corpus, 
-            total_examples=doc2vec.corpus_count,
-            epochs=doc2vec.epochs,
-            callbacks=[EpochLogger()])
-
-        indexer = AnnoyIndexer(doc2vec, self.index_trees)
-
-        self.doc2vec = doc2vec
-        self.indexer = indexer
+    def fit(self, embeddings):
+        self.emb_dim = len(embeddings[0])
+        self.indexer = self.make_indexer(embeddings)
 
     def lookup(self, text, n=10):
-        vector = self.doc2vec.infer_vector(tokenize_doc(text))
-        neighboors = self.doc2vec.wv.most_similar([vector], topn=n, indexer=self.indexer)
-        return neighboors
-
+        vector = self.model.infer([text])[0]
+        neighboor_idx = self.indexer.get_nns_by_vector(vector, n)
+        return neighboor_idx
+    
     def lookup_ids(self, text, n=10):
-        neighboors = self.lookup(text, n=n)
-        return [n[0] for n in neighboors]
+        return self.ids[self.lookup(text, n=n)]
+    
+    def lookup_texts(self, text, n=10):
+        return self.texts[self.lookup(text, n=n)]
 
     def save(self, dir_path):
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        pickle.dump(self.get_params_dict(), open( os.path.join(dir_path, 'emb.params'), "wb" ))
-        self.doc2vec.save(os.path.join(dir_path, 'emb.doc2vec'))
+        dill.dump(self.get_params_dict(), open( os.path.join(dir_path, 'emb.params'), "wb" ))
+        dill.dump(self.model.configuration_dict(), open(os.path.join(dir_path, 'emb.nnconfig'), "wb" ))
+        dill.dump(self.model.text_field, open( os.path.join(dir_path, 'emb.text_field'), "wb" ))
+        torch.save(self.model.state_dict(), os.path.join(dir_path, 'emb.pt'))
         self.indexer.save(os.path.join(dir_path, 'emb.annoy'))
 
     @classmethod
     def load(cls, dir_path):
-        params = pickle.load(open(os.path.join(dir_path, 'emb.params'), "rb" ))
-        doc2vec = Doc2Vec.load(os.path.join(dir_path, 'emb.doc2vec'))
-        indexer = AnnoyIndexer()
-        indexer.load(os.path.join(dir_path, 'emb.annoy'))
-        indexer.model = doc2vec
+        params = dill.load(open(os.path.join(dir_path, 'emb.params'), "rb" ))
         emb = Embedder()
         emb.set_params_dict(params)
-        emb.doc2vec = doc2vec
+        
+        configuration_dict = dill.load(open(os.path.join(dir_path, 'emb.nnconfig'), "rb" ))
+        text_field = dill.load(open(os.path.join(dir_path, 'emb.text_field'), "rb" ))
+        state_dict = torch.load(os.path.join(dir_path, 'emb.pt'))
+
+        model = LSTMEmbedder(text_field=text_field, **configuration_dict)
+        model.load_state_dict(state_dict)
+        model.eval()
+        
+        indexer = AnnoyIndex(emb.emb_dim, 'angular')
+        indexer.load(os.path.join(dir_path, 'emb.annoy'))
+        
+        emb.model = model
         emb.indexer = indexer
         return emb
